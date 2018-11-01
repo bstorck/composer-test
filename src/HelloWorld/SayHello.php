22f6bd744ebe2a48e3e788b49f42762a61e0cb7e
Brad Storck <me@bradstorck.com>
2018-11-01 16:40:21 -0600

    Vv

--- src/HelloWorld/SayHello.php
+++ src/HelloWorld/SayHello.php
@@ -5,6 +5,6 @@ class SayHello
 {
     public static function world()
     {
-        return 'Does this still update? Yes it does! Stil Updates? V.2';
+        return 'Does this still update? Yes it does! Stil Updates? V.6';
     }
 }
